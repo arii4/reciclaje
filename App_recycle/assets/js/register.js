b4134d4a15e0d20194648b4ad94deeb07f625f64
Pau Vila Capitan <pau.vila@fib.upc.edu>
2017-03-21 21:25:55 +0100

    APLICACIÓ

---
+++ App_recycle/assets/js/register.js
@@ -0,0 +1,41 @@
+// FUNCIO VALIDACIO EMAIL
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+// FUNCIO VALIDACIO PASSWORD
+function validatePassword(pass) {
+    var re = /^(?=.*[a-zA-Z])(?=.*[0-9])/;
+    return re.test(pass);
+}
+
+// FUNCIO ENSENYA ERROR EN POPUP
+function showAlert(title, subtitle, type){
+  var iserror = "error";
+  if(type == 'success') iserror = 'success';
+  swal(
+    title,
+    subtitle,
+    type
+  )
+}
+
+// FUNCIO CHECK ERRORS EN LOGIN
+$('#register-btn').on('click', function(){
+  if($('#password').val().length == 0 || $('#username').val().length == 0){
+    showAlert('Error', 'User i/o password sense valor', 'error');
+  }
+  else if( validateEmail($('#username').val()) == false ){
+    showAlert('Error', 'Email no valid', 'error');
+  }
+  else if($('#password').val().length < 8){
+    showAlert('Error', 'Password massa curta', 'error');
+  }
+  else if( validatePassword($('#password').val()) == false ){
+    showAlert('Error', 'La password ha de contenir nombres i lletres', 'error');
+  }
+  else{
+    window.location.replace('index.html');
+  }
+});
